432f08ea6c147f39d96dae3beea591eaf6be8462
wrichter@redhat.com <wrichter@redhat.com>
2015-03-04 20:28:16 +0000

    update file /application.js

--- application.js
+++ application.js
@@ -26,6 +26,7 @@ app.use(mbaasExpress.fhmiddleware());
 
 // fhlint-begin: custom-routes
 app.use('/hello', require('./lib/hello.js')());
+app.use('/create', require('./lib/create.js')());
 // fhlint-end
 
 // Important that this is last!
